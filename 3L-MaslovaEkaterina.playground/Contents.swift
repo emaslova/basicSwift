3f71e7087aa631f0db99ae4bc3917ad0a910cca8
Ekaterina <emaslova@yandex.ru>
2019-08-18 19:26:30 +0300

    Вроде бы сделанное ДЗ

--- 3L-MaslovaEkaterina.playground/Contents.swift
+++ 3L-MaslovaEkaterina.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 enum CarBody: String {
     case sedan = "Седан"
@@ -8,6 +9,16 @@ enum CarBody: String {
     case coupe = "Купе"
     case pickup = "Пикап"
     case truck = "Грузовик"
+}
+
+enum CarColor: String { // цвет машины задан не через UIColor, потому что его нельзя через rawValue красиво напечатать в консоли, получается Цвет машины: UIExtendedSRGBColorSpace 1 0.5 0 1 , а хочется чтобы было красиво :))
+    case white = "Белый"
+    case black = "Чёрный"
+    case red = "Красный"
+    case pink = "Розовый"
+    case green = "Зелёный"
+    case purple = "Фиолетовый"
+    case silver = "Серебристый"
 }
 
 enum DoorOpen: String {
@@ -44,24 +55,50 @@ struct Car {
     let carBrand: String
     let carBody: CarBody
     let yearOfIssue: Int
-    let carcolor:  
+    let carColor: CarColor
     var km: Double
     var transmission: Transmission
     var trunkVolume: TrunkVolume
     var engineRunning: EngineRunning
-    var windowsOpen: WindowsOpen
+    var windowsOpen: WindowsOpen {
+        willSet {
+            if newValue == .openDriversWindow {
+                print("Курить нехорошо! Особенно в машине!")
+            } else {
+                print("Вы молодец, что не курите в машине!")
+            }
+        }
+    }
+    
     var doorOpen: DoorOpen
     var trunkFilledUp: Int
     
+    
+    
     var description: String {
-        return "Марка машины: \(carBrand)\nКузов автомобиля: \(carBody.rawValue)\nГод выпуска: \(yearOfIssue) \nЦвет машины: \(carcolor)\nПробег: \(km) \nКоробка передач: \(transmission.rawValue) \nОбъём багажника: \(trunkVolume.rawValue) \nДвигатель в положении: \(engineRunning) \nОкна: \(windowsOpen.rawValue) \nДвери в положении: \(doorOpen.rawValue) \nЗаполненный объем багажника: \(trunkFilledUp)%"
+        get {
+            return "Марка машины: \(carBrand)\nКузов автомобиля: \(carBody.rawValue)\nГод выпуска: \(yearOfIssue) \nЦвет машины: \(carColor.rawValue)\nПробег: \(km) \nКоробка передач: \(transmission.rawValue) \nОбъём багажника: \(trunkVolume.rawValue) \nДвигатель в положении: \(engineRunning) \nОкна: \(windowsOpen.rawValue) \nДвери в положении: \(doorOpen.rawValue) \nЗаполненный объем багажника: \(trunkFilledUp)%"
+        }
+        set {
+            if let newKm = Double(newValue) {
+                self.km = newKm
+            }
+        }
     }
 }
 
-var car1 = Car(carBrand: "BMW", carBody: .coupe, yearOfIssue: 2019, carcolor: .orange, km: 210.50, transmission: .auto, trunkVolume: .middle, engineRunning: .stop, windowsOpen: .closed, doorOpen: .close, trunkFilledUp: 50)
+var car1 = Car(carBrand: "Ситроен", carBody: .hatchback, yearOfIssue: 2011, carColor: .black, km: 149111.0, transmission: .auto, trunkVolume: .extraSmall, engineRunning: .stop, windowsOpen: .closed, doorOpen: .close, trunkFilledUp: 50)
 
+var car2 = Car(carBrand: "Какая-то большая фура", carBody: .truck, yearOfIssue: 2018, carColor: .silver, km: 350432.67, transmission: .manual, trunkVolume: .extraLarge, engineRunning: .start, windowsOpen: .open, doorOpen: .close, trunkFilledUp: 40)
+
+print("ОПИСАНИЕ МОЕЙ МАШИНЫ:")
 print(car1.description)
-
-
-
-
+car1.description = "150000.0"
+print("")
+print(car1.description)
+print("")
+print("ОПИСАНИЕ ГРУЗОВИКА:")
+print(car2.description)
+car2.windowsOpen = .openDriversWindow
+print("")
+print(car2.description)
