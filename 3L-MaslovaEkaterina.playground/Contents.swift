a51080e2f1bd08ad7b02e02466ef739888c9be22
Ekaterina <emaslova@yandex.ru>
2019-08-22 02:38:57 +0300

    4L-MaslovaEkaterina

--- 3L-MaslovaEkaterina.playground/Contents.swift
+++ 3L-MaslovaEkaterina.playground/Contents.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Foundation
 
 enum CarBody: String {
     case sedan = "Седан"
@@ -11,7 +10,7 @@ enum CarBody: String {
     case truck = "Грузовик"
 }
 
-enum CarColor: String { // цвет машины задан не через UIColor, потому что его нельзя через rawValue красиво напечатать в консоли, получается Цвет машины: UIExtendedSRGBColorSpace 1 0.5 0 1 , а хочется чтобы было красиво :))
+enum CarColor: String {
     case white = "Белый"
     case black = "Чёрный"
     case red = "Красный"
@@ -50,7 +49,7 @@ enum TrunkVolume: String {
 }
 
 
-struct Car {
+class Car {
     
     let carBrand: String
     let carBody: CarBody
@@ -73,8 +72,6 @@ struct Car {
     var doorOpen: DoorOpen
     var trunkFilledUp: Int
     
-    
-    
     var description: String {
         get {
             return "Марка машины: \(carBrand)\nКузов автомобиля: \(carBody.rawValue)\nГод выпуска: \(yearOfIssue) \nЦвет машины: \(carColor.rawValue)\nПробег: \(km) \nКоробка передач: \(transmission.rawValue) \nОбъём багажника: \(trunkVolume.rawValue) \nДвигатель в положении: \(engineRunning) \nОкна: \(windowsOpen.rawValue) \nДвери в положении: \(doorOpen.rawValue) \nЗаполненный объем багажника: \(trunkFilledUp)%"
@@ -85,20 +82,129 @@ struct Car {
             }
         }
     }
+    
+    init(carBrand: String, carBody: CarBody, yearOfIssue: Int, carColor: CarColor, km: Double, transmission: Transmission, trunkVolume: TrunkVolume, engineRunning: EngineRunning, windowsOpen: WindowsOpen, doorOpen: DoorOpen, trunkFilledUp: Int) {
+        self.carBrand = carBrand
+        self.carBody = carBody
+        self.yearOfIssue = yearOfIssue
+        self.carColor = carColor
+        self.km = km
+        self.transmission = transmission
+        self.trunkVolume = trunkVolume
+        self .engineRunning = engineRunning
+        self.windowsOpen = windowsOpen
+        self.doorOpen = doorOpen
+        self.trunkFilledUp = trunkFilledUp
+    }
+    
+    func goToDrive() {
+        var newKm = km
+        newKm += 100.0
+        km = newKm
+        
+        print("Пробег увеличен на 100 км")
+    }
+    
 }
 
-var car1 = Car(carBrand: "Ситроен", carBody: .hatchback, yearOfIssue: 2011, carColor: .black, km: 149111.0, transmission: .auto, trunkVolume: .extraSmall, engineRunning: .stop, windowsOpen: .closed, doorOpen: .close, trunkFilledUp: 50)
 
-var car2 = Car(carBrand: "Какая-то большая фура", carBody: .truck, yearOfIssue: 2018, carColor: .silver, km: 350432.67, transmission: .manual, trunkVolume: .extraLarge, engineRunning: .start, windowsOpen: .open, doorOpen: .close, trunkFilledUp: 40)
+class SportCar: Car {
+    
+    weak var transported: TrunkCar?
+    var spoiler: String
+    var sportsSuspension: String
+    var sportsBrakeSystem: String
+    var sportsWheels: Int
+    
+    override var description: String {
+        get {
+            return "Марка машины: \(carBrand)\nКузов автомобиля: \(carBody.rawValue)\nГод выпуска: \(yearOfIssue) \nЦвет машины: \(carColor.rawValue)\nПробег: \(km) \nКоробка передач: \(transmission.rawValue) \nОбъём багажника: \(trunkVolume.rawValue) \nДвигатель в положении: \(engineRunning) \nОкна: \(windowsOpen.rawValue) \nДвери в положении: \(doorOpen.rawValue) \nЗаполненный объем багажника: \(trunkFilledUp)% \nМашина перевозится с помощью: \(transported?.carBrand ?? "не транспортируется, едет сама") \nСпойлер: \(spoiler) \nСпортивная подвеска: \(sportsSuspension) \nСпортивная тормозная система: \(sportsBrakeSystem) \nСпортивные колёса: \(sportsWheels)"
+        }
+        set {
+            if let newKm = Double(newValue) {
+                self.km = newKm
+            }
+        }
+    }
+    
+    init(carBrand: String, carBody: CarBody, yearOfIssue: Int, carColor: CarColor, km: Double, transmission: Transmission, trunkVolume: TrunkVolume, engineRunning: EngineRunning, windowsOpen: WindowsOpen, doorOpen: DoorOpen, trunkFilledUp: Int, transported: TrunkCar? = nil, spoiler: String, sportsSuspension: String, sportsBrakeSystem: String, sportsWheels: Int) {
+        
+        self.transported = transported
+        self.spoiler = spoiler
+        self.sportsSuspension = sportsSuspension
+        self.sportsBrakeSystem = sportsBrakeSystem
+        self.sportsWheels = sportsWheels
+        super.init(carBrand: carBrand, carBody: carBody, yearOfIssue: yearOfIssue, carColor: carColor, km: km, transmission: transmission, trunkVolume: trunkVolume, engineRunning: engineRunning, windowsOpen: windowsOpen, doorOpen: doorOpen, trunkFilledUp: trunkFilledUp)
+        }
+    
+    override func goToDrive() {
+        super.goToDrive()
+        print("Слишком быстро не гоняй!")
+    }
+    
+    func takesUpgradeInWorkshop() {
+        let newSportsWheels = sportsWheels
+        newSportsWheels == 4
+        sportsWheels = newSportsWheels
+        print("У вашего спорткара 4 новых супер-спортивных колеса.")
+    }
+}
 
-print("ОПИСАНИЕ МОЕЙ МАШИНЫ:")
-print(car1.description)
-car1.description = "150000.0"
-print("")
-print(car1.description)
+class TrunkCar: Car {
+    
+    unowned let transports: SportCar
+    var trailer: String
+    var trunkWheels: Int
+    
+    override var description: String {
+        get {
+            return "Марка машины: \(carBrand)\nКузов автомобиля: \(carBody.rawValue)\nГод выпуска: \(yearOfIssue) \nЦвет машины: \(carColor.rawValue)\nПробег: \(km) \nКоробка передач: \(transmission.rawValue) \nОбъём багажника: \(trunkVolume.rawValue) \nДвигатель в положении: \(engineRunning) \nОкна: \(windowsOpen.rawValue) \nДвери в положении: \(doorOpen.rawValue) \nЗаполненный объем багажника: \(trunkFilledUp)% \nПеревозит: \(transports.carBrand) \nПрицеп: \(trailer) \nКолёса: \(trunkWheels)"
+        }
+        set {
+            if let newKm = Double(newValue) {
+                self.km = newKm
+            }
+        }
+    }
+    
+    init(carBrand: String, carBody: CarBody, yearOfIssue: Int, carColor: CarColor, km: Double, transmission: Transmission, trunkVolume: TrunkVolume, engineRunning: EngineRunning, windowsOpen: WindowsOpen, doorOpen: DoorOpen, trunkFilledUp: Int, transports: SportCar, trailer: String, trunkWheels: Int) {
+        
+        self.transports = transports
+        self.trailer = trailer
+        self.trunkWheels = trunkWheels
+        super.init(carBrand: carBrand, carBody: carBody, yearOfIssue: yearOfIssue, carColor: carColor, km: km, transmission: transmission, trunkVolume: trunkVolume, engineRunning: engineRunning, windowsOpen: windowsOpen, doorOpen: doorOpen, trunkFilledUp: trunkFilledUp)
+    }
+    
+    override func goToDrive() {
+        super.goToDrive()
+        print("Доставка будет вовремя!")
+    }
+    
+    func makeSafer() {
+        var newTrunkWheels = trunkWheels
+        newTrunkWheels += 2
+        trunkWheels = newTrunkWheels
+        print("Добавлено 2 запасных колеса. Грузовик стал безопаснее.")
+    }
+    
+}
+
+
+
+
+
+var sportCar: SportCar? = SportCar(carBrand: "Porsche GT3 RS", carBody: .coupe, yearOfIssue: 2015, carColor: .red, km: 20000.0, transmission: .manual, trunkVolume: .extraSmall, engineRunning: .stop, windowsOpen: .closed, doorOpen: .close, trunkFilledUp: 0, spoiler: "Есть", sportsSuspension: "установлена", sportsBrakeSystem: "Заводская комплектация", sportsWheels: 4)
+
+var trunkCar: TrunkCar? = TrunkCar(carBrand: "Mercedes-Benz Actros", carBody: .truck, yearOfIssue: 2011, carColor: .silver, km: 350000.0, transmission: .auto, trunkVolume: .extraLarge, engineRunning: .start, windowsOpen: .openDriversWindow, doorOpen: .close, trunkFilledUp: 50, transports: sportCar!, trailer: "отсутствует", trunkWheels: 6)
+
+sportCar!.transported = trunkCar
+
+print("ОПИСАНИЕ СПОРТКАРА:")
+print(sportCar!.description)
+sportCar!.goToDrive()
+sportCar!.takesUpgradeInWorkshop()
 print("")
 print("ОПИСАНИЕ ГРУЗОВИКА:")
-print(car2.description)
-car2.windowsOpen = .openDriversWindow
-print("")
-print(car2.description)
+print(trunkCar!.description)
+trunkCar!.goToDrive()
+trunkCar!.makeSafer()
